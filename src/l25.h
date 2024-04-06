a1d342d0c877137e75637b77b93eecbcd233ae52
Larsouille25 <superthithi2008@gmail.com>
2024-04-06 19:51:28 +0200

    create a simple header library named "l25.h" used to do what C does not

---
+++ src/l25.h
@@ -0,0 +1,170 @@
+/*
+Larsouille25's c library, "l25.h", it's a very simple one file library
+containing useful things, like colors, strings, string manipulation,
+dynamic arrays and maybe allocators.
+*/
+
+#ifndef L25_H_
+#define L25_H_
+
+#include <inttypes.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum  {
+	RESET_STYLE = 0,
+	BOLD_STYLE = 1,
+	ITALIC_STYLE = 3,
+	UNDERLINE_STYLE = 4,
+
+	BLACK_FG_COLOR = 30,
+	RED_FG_COLOR = 31,
+	GREEN_FG_COLOR = 32,
+	YELLOW_FG_COLOR = 33,
+	BLUE_FG_COLOR = 34,
+	MAGENTA_FG_COLOR = 35,
+	CYAN_FG_COLOR = 36,
+	WHITE_FG_COLOR = 37,
+
+	BLACK_BG_COLOR = 40,
+	RED_BG_COLOR = 41,
+	GREEN_BG_COLOR = 42,
+	YELLOW_BG_COLOR = 43,
+	BLUE_BG_COLOR = 44,
+	MAGENTA_BG_COLOR = 45,
+	CYAN_BG_COLOR = 46,
+	WHITE_BG_COLOR = 47,
+} L25_Style;
+
+#define BRIGHTEN_COLOR(color) ((color) + 60)
+
+#define set_style_two(stream, lhs, rhs) fprintf(stream, "\x1B[%d;%dm", lhs, rhs);
+#define set_style(stream, style) fprintf(stream, "\x1B[%dm", style);
+#define reset_style(stream) fprintf(stream, "\x1B[0;0m");
+
+typedef enum {
+	L25_OK,
+	L25_ERROR,
+	L25_ALLOC_ERR,
+	L25_BOUNDARY_ERR,
+} L25_Res;
+
+typedef struct {
+	const char* str;
+	size_t len;
+} L25_StringSlice;
+
+// Converts a null terminated string into a string slice.
+L25_StringSlice l25_cstr_to_ss(char* c_str);
+// Write to the stream the string slice.
+void l25_fputss(L25_StringSlice ss, FILE* stream);
+
+typedef struct {
+	char *str;
+	size_t len;
+	size_t cap;
+} L25_DynString;
+
+// Initiate a growable string and allocating the capacity passed in arg.
+L25_Res l25_ds_init(L25_DynString* ds, size_t cap);
+// Push a character to the string, allocates more memory if necessary.
+L25_Res l25_ds_push(L25_DynString* ds, char c);
+// Appends the string slice to the dynamic string and allocates memory if
+// necesseray.
+L25_Res l25_ds_append_ss(L25_DynString* ds, L25_StringSlice ss);
+// Converts a dynamic string into a String Slice.
+L25_Res l25_ds_to_ss(L25_DynString* ds, L25_StringSlice* ss);
+// Take a slice of the dynamic string and return a string slice of it.
+L25_StringSlice l25_ds_slice(L25_DynString* ds, size_t start, size_t end);
+// Frees the memory and put zeros in all fields.
+void l25_ds_free(L25_DynString* ds);
+// Converts a null terminated string into a dynamic string.
+L25_Res l25_ds_from_cstr(L25_DynString* ds, char* cstr);
+// Write to the stream the string slice.
+void l25_fputds(L25_DynString* ds, FILE* stream);
+
+#endif // L25_H_
+
+#ifdef L25_IMPL
+
+L25_StringSlice l25_cstr_to_ss(char* cstr) {
+	return (L25_StringSlice){.str = cstr, .len = strlen(cstr)};
+}
+
+void l25_fputss(L25_StringSlice ss, FILE* stream) {
+	for (size_t i = 0; i <= ss.len; i++) {
+		fputc(ss.str[i], stream);
+	}
+}
+
+void l25_fputds(L25_DynString* ds, FILE* stream) {
+	for (size_t i = 0; i <= ds->len; i++) {
+		fputc(ds->str[i], stream);
+	}
+}
+
+L25_Res l25_ds_init(L25_DynString* ds, size_t cap) {
+	ds->str = (char*)malloc(cap * sizeof(char));
+	if (!ds->str) {
+		return L25_ALLOC_ERR;
+	}
+	ds->len = 0;
+	ds->cap = cap;
+	return L25_OK;
+}
+
+L25_Res l25_ds_push(L25_DynString* ds, char c) {
+	if (ds->len >= ds->cap) {
+		size_t newcap = ds->cap * 2;
+		char* newstr = (char*)realloc(ds->str, newcap * sizeof(char));
+
+		if (!ds->str) {
+			return L25_ALLOC_ERR;
+		}
+
+		ds->str = newstr;
+		ds->cap = newcap;
+	}
+	ds->str[ds->len++] = c;
+	return L25_OK;
+}
+
+L25_Res l25_ds_append_ss(L25_DynString* ds, L25_StringSlice ss) {
+	size_t new_len = ds->len + ss.len;
+	if (ds->cap > new_len) {
+		char* newstr = (char*)realloc(ds->str, new_len * sizeof(char));
+
+		if (!ds->str) {
+			return L25_ALLOC_ERR;
+		}
+
+		ds->str = newstr;
+		ds->cap = new_len;
+	}
+	memcpy(ds->str + ds->len, ss.str, ss.len);
+	ds->len = new_len;
+	return L25_OK;
+}
+
+L25_Res l25_ds_from_cstr(L25_DynString* ds, char* cstr) {
+	ds->len = strlen(cstr);
+	ds->cap = ds->len;
+	ds->str = (char*) malloc(ds->cap * sizeof(char));
+	if (!ds->str) {
+		return L25_ALLOC_ERR;
+	}
+	memcpy(ds->str, cstr, ds->cap);
+	return L25_OK;
+}
+
+void l25_ds_free(L25_DynString* ds) {
+	ds->len = 0;
+	ds->cap = 0;
+	free(ds->str);
+	ds->str = NULL;
+}
+
+#endif // L25_IMPL
